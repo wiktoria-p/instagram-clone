0dde8c3c88324d0b05c7f395d87908073870f31e
wiktoria-p <pietrzykwiktoria01@gmail.com>
2021-04-27 15:52:42 +0200

    add heart color change

---
+++ script.js
@@ -0,0 +1,9 @@
+function  likePost(){
+  let heart = document.getElementById('1');
+  if ( heart.style.color=="black" ) {
+    heart.style.color="red";
+  } else {
+    heart.style.color="black";
+  }
+}
+
