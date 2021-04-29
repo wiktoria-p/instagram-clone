99670281928c40e3e8b8832b7d34b277b88a07a8
Wiktoria Pietrzyk <pietrzykwiktoria01@gmail.com>
2021-04-29 22:46:06 +0200

    add shortcut profile

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ photo.addEventListener("dblclick", likePost);
 function  likePost(){
   heart.classList.toggle("liked");
 }
+
+let profilePhoto = document.getElementById("3");
+
+
+function clickProfile(){
+  console.log("jest")
+  let shortProfile = document.getElementById("4");
+  shortProfile.classList.toggle("active");
+  shortProfile.classList.toggle("shortcut");
+}
+
+profilePhoto.addEventListener("click", clickProfile);
