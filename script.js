0ad93fa4352fa4620cff3393bafc2878917ea5a0
Wiktoria Pietrzyk <pietrzykwiktoria01@gmail.com>
2021-05-10 00:02:15 +0200

    add random entries and user names to posts

--- script.js
+++ script.js
@@ -26,25 +26,36 @@ function clickProfile(){
 profilePhoto.addEventListener("click", clickProfile)
 
 let timeline = document.getElementById("timeline")
+let names = ["ladydrama123", "blueberries", "lamadrama", "lordvader550", "martynka.official", "mamacita09", "henio78", "blackwidow", "marry.sol", "baby23254", "milenka", "angel345", "sexylola", "blabla"]
+let descriptions = ["life is life", "love is life", "bamboo is life", "i love my dog", "butterfly is beautyful", "coronavirus is fucking fake", "my cat is very sweet", "lalalalalala", "my new tatoo", "my new car", "weeding is coming", "i say yes", "i love my mom"]
 
-function addPost(name, description){
+// the scroll event
+// scrollHeight= height of element
+// scrollTop= offset from the top of the page
+// clientHeight= height of window view
+
+window.addEventListener('scroll',()=>{
+	const {scrollHeight,scrollTop,clientHeight} = document.documentElement;
+	if(scrollTop + clientHeight > scrollHeight - 5){
+		setTimeout(addPost,500);
+
+	}
+});
+
+// add post with users entry
+function addPost(name = false, description = false){
   let blueprint = document.getElementById("post_blueprint").cloneNode(true)
   let userName = blueprint.getElementsByClassName("username")[0]
   let entry = blueprint.getElementsByClassName("description")[0]
+  let photo = blueprint.getElementsByClassName("photo")[0]
+  description ||= descriptions[Math.floor(Math.random()*descriptions.length)]
   entry.innerHTML = description
+  name ||= names[Math.floor(Math.random()*names.length)]
   userName.innerHTML = name
+  photo.src = "https://picsum.photos/500"
   timeline.appendChild(blueprint)
   blueprint.classList.toggle("blueprint")
 }
 
-addPost("blablabla", "ljkhjhlhjvb")
-addPost("alalalala", "gvgcgcg")
-
-
-// let publish = document.getElementsByClassName("com")
-
-// function addComment(commentery){
-  
-// }
 
 
